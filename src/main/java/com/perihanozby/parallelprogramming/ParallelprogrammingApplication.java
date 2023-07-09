62933d9fbfa3fc50af485e5da1b893bd6e3f52f0
Perihan Ozbay <85580340+perihanozby@users.noreply.github.com>
2023-07-09 14:16:16 +0300

    bu kod parçası, kullanıcıdan alınan verileri iki ayrı dosyaya yazmayı hedefleyen çoklu iş parçacıklı bir programdır.

---
+++ src/main/java/com/perihanozby/parallelprogramming/ParallelprogrammingApplication.java
@@ -0,0 +1,13 @@
+package com.perihanozby.parallelprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParallelprogrammingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParallelprogrammingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/perihanozby/parallelprogramming/ParallelprogrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perihanozby.parallelprogramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParallelprogrammingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
